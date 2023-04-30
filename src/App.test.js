a159e3be35f7ac2a2b93cedf9c52df5ee64ac272
Marshal Nadar <marshalnadarmn18@gmail.com>
2023-04-30 15:09:51 +0530

    Without waitFor() event

---
+++ src/App.test.js
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import App from "./App";
+
+test("can receive a new user and show it on a list", () => {
+  render(<App />);
+  // const { container } = render(<App />);
+
+  const nameInput = screen.getByRole("textbox", {
+    name: /name/i,
+  });
+  const emailInput = screen.getByRole("textbox", {
+    name: /email/i,
+  });
+  const button = screen.getByRole("button");
+
+  user.click(nameInput);
+  user.keyboard("jane");
+
+  user.click(emailInput);
+  user.keyboard("micheal@micheal");
+
+  //Working
+  // fireEvent.click(container.querySelector(".btn-how-to-choose-provider"));
+
+  user.click(button);
+
+  // screen.debug();
+
+  // By using 'row'
+  // const another = screen.getByRole("row", {
+  //   name: /jane micheal@micheal/i,
+  // });
+
+  const name = screen.getByRole("cell", { name: "jane" });
+  const email = screen.getByRole("cell", { name: "micheal@micheal" });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+  // expect(another).toBeInTheDocument();
+});
